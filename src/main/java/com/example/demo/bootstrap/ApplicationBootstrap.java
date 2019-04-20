733413172eb1d45bc75141d5b2c18623576f9aa0
Shabdan <shabdan.abjaparov.official@gmail.com>
2019-04-20 18:47:21 +0600

    fixed some details

--- src/main/java/com/example/demo/bootstrap/ApplicationBootstrap.java
+++ src/main/java/com/example/demo/bootstrap/ApplicationBootstrap.java
@@ -19,16 +19,16 @@ public class ApplicationBootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Person p1 = new Person("Shabdan",17);
-        Application ap1 = new Application(p1, LocalDateTime.now(),"Hey new Application");
-        Application ap2 = new Application(p1, LocalDateTime.now(),"Hey second new Application");
-        Application ap3 = new Application(p1, LocalDateTime.now(),"Hey third new Application");
-        Application ap4 = new Application(p1, LocalDateTime.now(),"Hey fourth new Application");
-        personService.createPerson(p1);
-
-        applicationService.createApplication(ap1);
-        applicationService.createApplication(ap2);
-        applicationService.createApplication(ap3);
-        applicationService.createApplication(ap4);
+//        Person p1 = new Person("Shabdan",17);
+//        Application ap1 = new Application(p1, LocalDateTime.now(),"Hey new Application");
+//        Application ap2 = new Application(p1, LocalDateTime.now(),"Hey second new Application");
+//        Application ap3 = new Application(p1, LocalDateTime.now(),"Hey third new Application");
+//        Application ap4 = new Application(p1, LocalDateTime.now(),"Hey fourth new Application");
+//        personService.createPerson(p1);
+//
+//        applicationService.createApplication(ap1);
+//        applicationService.createApplication(ap2);
+//        applicationService.createApplication(ap3);
+//        applicationService.createApplication(ap4);
     }
 }
